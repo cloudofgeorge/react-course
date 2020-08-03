86eadcac7e989205cee8b0561136a38edbcb7393
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-08-03 22:33:53 +0300

    implement state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Product from "./Product";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>
-          Hello world
-        </h1>
-      </header>
-      <Product name='Avocado' count='10'>
-          Avocados make just about anything better, from salads to veggie-packed salsas to smoothies. Smear avocados onto toast, mash 'em into guacamole, or top your morning eggs with a couple slices
-      </Product>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    products: [
+      { name: "Avocado", count: 10 },
+      { name: "Bread", count: 5 },
+    ],
+  };
+
+  render() {
+    const products = this.state.products;
+
+    return (
+      <div className="App">
+        <header>
+          <h1>Hello world</h1>
+        </header>
+        <Product name={products[0].name} count={products[0].count} />
+        <Product name={products[1].name} count={products[1].count} />
+      </div>
+    );
+  }
 }
 
 export default App;
