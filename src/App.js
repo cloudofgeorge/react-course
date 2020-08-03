16f5112d5b570ae8548255b056a1d623f33510d8
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-08-03 23:10:21 +0300

    implement lists

--- src/App.js
+++ src/App.js
@@ -17,32 +17,32 @@ class App extends React.Component {
   };
 
   addToCartHandler = (id, name) => {
-    console.log({ id, name });
+    this.setState((prevState) => ({ cart: [...prevState.cart, { id, name }] }));
   };
 
   render() {
-    const products = this.state.products;
-
     return (
       <div className="App">
         <header>
           <h1>{this.state.pageTitle}</h1>
         </header>
         <button onClick={this.changePageTitleHandler}>Change title</button>
-        <Product
-          name={products[0].name}
-          count={products[0].count}
-          addToCart={() =>
-            this.addToCartHandler(products[0].id, products[0].name)
-          }
-        />
-        <Product
-          name={products[1].name}
-          count={products[1].count}
-          addToCart={() =>
-            this.addToCartHandler(products[1].id, products[1].name)
-          }
-        />
+        <div>
+          {this.state.products.map((item) => (
+            <Product
+              key={item.id}
+              name={item.name}
+              count={item.count}
+              addToCart={() => this.addToCartHandler(item.id, item.name)}
+            />
+          ))}
+        </div>
+        <div>
+          <div>Cart:</div>
+          {this.state.cart.map((item) => (
+            <div>{item.name}</div>
+          ))}
+        </div>
       </div>
     );
   }
