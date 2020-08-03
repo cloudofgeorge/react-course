a664e3e710bd95bf313f4b416be7dfe1c2d15947
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-08-03 23:03:08 +0300

    implement work with events

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ class App extends React.Component {
   state = {
     pageTitle: "My App",
     products: [
-      { name: "Avocado", count: 10 },
-      { name: "Bread", count: 5 },
+      { id: 0, name: "Avocado", count: 10 },
+      { id: 1, name: "Bread", count: 5 },
     ],
+    cart: [],
   };
 
   changePageTitleHandler = () => {
     this.setState({ pageTitle: "Products list" });
+  };
+
+  addToCartHandler = (id, name) => {
+    console.log({ id, name });
   };
 
   render() {
@@ -24,8 +29,20 @@ class App extends React.Component {
           <h1>{this.state.pageTitle}</h1>
         </header>
         <button onClick={this.changePageTitleHandler}>Change title</button>
-        <Product name={products[0].name} count={products[0].count} />
-        <Product name={products[1].name} count={products[1].count} />
+        <Product
+          name={products[0].name}
+          count={products[0].count}
+          addToCart={() =>
+            this.addToCartHandler(products[0].id, products[0].name)
+          }
+        />
+        <Product
+          name={products[1].name}
+          count={products[1].count}
+          addToCart={() =>
+            this.addToCartHandler(products[1].id, products[1].name)
+          }
+        />
       </div>
     );
   }
--- src/Product.js
+++ src/Product.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
-const Product = props => (
-    <div className="product">
-        <div>Product: {props.name}</div>
-        <div>Count: {props.count}</div>
-        <div>description: {props.children}</div>
-        <button>Buy</button>
-    </div>
+const Product = (props) => (
+  <div className="product">
+    <div>Product: {props.name}</div>
+    <div>Count: {props.count}</div>
+    <div>description: {props.children}</div>
+    <button onClick={props.addToCart}>Buy</button>
+  </div>
 );
 
 export default Product;
