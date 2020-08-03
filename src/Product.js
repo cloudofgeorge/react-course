9a170b66c6b2875f587bc4e52c413783107a445e
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-08-03 21:55:36 +0300

    implement simple component

---
+++ src/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Product.css';
+
+const Product = () => (
+    <div className="product">
+        <div>This is product</div>
+        <button>Buy</button>
+    </div>
+);
+
+export default Product;
