7e5b4230737b6290a0e74a37f2e3ca6330016775
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-09-13 21:06:33 +0300

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Product = props => (
-    <div className="product">
+    <div>
         <div>Product: {props.name}</div>
         <div>Count: {props.count}</div>
         <button>Buy</button>
