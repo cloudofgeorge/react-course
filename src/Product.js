d6d84ccf013a4df810a4755c3780aaeaa5ea5cbb
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-09-13 21:09:51 +0300

    remove Product styles

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import './Product.css';
 
 const Product = props => (
-    <div className="product">
+    <div>
         <div>Product: {props.name}</div>
         <div>Count: {props.count}</div>
         <div>description: {props.children}</div>
