d069d9a4fcd7fb56af6df95050b7f9323f1d7bba
cloudofgeorge <cloudofgeorge@gmail.com>
2020-12-01 17:46:14 +0300

    implement useState

--- src/components/common/header/cart/cart.js
+++ src/components/common/header/cart/cart.js
@@ -1,53 +1,42 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styles from "./cart.module.scss";
 import { removeCartItemAction } from "../../../../store/cart";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCartData } from "../../../../store/cart";
 
-class CartView extends React.Component {
-  state = {
-    isShow: false,
-  };
+export const Cart = () => {
+  const [isShow, setIsShow] = useState(false);
 
-  toggleWindow = () => {
-    const { cartData } = this.props;
+  const cartData = useSelector(getCartData);
 
+  const dispatch = useDispatch();
+  const removeCartItem = useCallback(
+    (catalogName) => dispatch(removeCartItemAction(catalogName)),
+    [dispatch]
+  );
+
+  const toggleWindow = () => {
     if (cartData && cartData.length) {
-      this.setState((prevState) => ({ isShow: !prevState.isShow }));
+      setIsShow(!isShow);
     }
   };
 
-  render() {
-    const { isShow } = this.state;
-    const { cartData, removeCartItem } = this.props;
+  return (
+    <div className={styles.cart}>
+      <button onClick={toggleWindow} disabled={cartData.length === 0}>
+        cart {cartData.length > 0 && cartData.length}
+      </button>
 
-    return (
-      <div className={styles.cart}>
-        <button onClick={this.toggleWindow} disabled={cartData.length === 0}>
-          cart {cartData.length > 0 && cartData.length}
-        </button>
-
-        {isShow && cartData.length > 0 && (
-          <div className={styles.cartWindow}>
-            {cartData.map((item) => (
-              <div key={item.id} className={styles.cartItem}>
-                <span className={styles.cartItemName}>{item.title}</span>
-                <button onClick={() => removeCartItem(item)}>x</button>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  cartData: getCartData(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  removeCartItem: (catalogName) => dispatch(removeCartItemAction(catalogName)),
-});
-
-export const Cart = connect(mapStateToProps, mapDispatchToProps)(CartView);
+      {isShow && cartData.length > 0 && (
+        <div className={styles.cartWindow}>
+          {cartData.map((item) => (
+            <div key={item.id} className={styles.cartItem}>
+              <span className={styles.cartItemName}>{item.title}</span>
+              <button onClick={() => removeCartItem(item)}>x</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,4 @@
 import React from "react";
 import { Layout } from "../components/common/layout";
 
-class MainPageView extends React.Component {
-  render() {
-    return <Layout>Welcome to our shop</Layout>;
-  }
-}
-
-export const MainPage = MainPageView;
+export const MainPage = () => <Layout>Welcome to our shop</Layout>;
